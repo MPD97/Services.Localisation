ea04df8529da48f38ca76deecd846d8cce2bd7c7
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 18:16:07 +0200

    Created service implementations

---
+++ src/Services.Localisation.Infrastructure/Services/DateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Services.Localisation.Application.Services;
+
+namespace Services.Localisation.Infrastructure.Services
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now  => DateTime.UtcNow;
+    }
+}
