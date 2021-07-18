ee6a2b89a0102f111ca4aced45297ee9b708a841
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 13:18:42 +0200

    Created dto

---
+++ src/Services.Localisation.Application/DTO/LocationDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Services.Localisation.Core.Entities;
+
+namespace Services.Localisation.Application.DTO
+{
+    public class LocationDto
+    {
+        public Guid Id { get; set; }  
+        public Guid UserId { get; set; }  
+        public DateTime CreatedAt { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public int  Accuracy { get; set; }
+
+        public LocationDto()
+        {
+            
+        }
+
+        public LocationDto(Location location)
+        {
+            Id = location.Id;
+            UserId = location.UserId;
+            CreatedAt = location.CreatedAt;
+            Latitude = location.Latitude;
+            Longitude = location.Longitude;
+            Accuracy = location.Accuracy;
+        }
+    }
+}
