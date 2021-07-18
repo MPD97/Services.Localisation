168cf5494239efd0c6ea6c89b88a32cb094150e4
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 12:36:08 +0200

    Created exception

---
+++ src/Services.Localisation.Core/Exceptions/InvalidAccuracyException.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Services.Localisation.Core.Exceptions
+{
+    public class InvalidAccuracyException : DomainException
+    {
+        public override string Code { get; } = "invalid_accuracy";
+        
+        public decimal Accuracy { get; }
+        public Guid UserId { get; }
+
+        public InvalidAccuracyException(Guid userId, decimal accuracy) 
+            : base($"Location for user: {userId} has invalid accuracy: {accuracy}.")
+        {
+        }
+    }
+}
