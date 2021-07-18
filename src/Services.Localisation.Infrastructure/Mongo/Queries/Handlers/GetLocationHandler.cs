d4c9c8e5e326f1c4dedabd382fb5ab1452079db4
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 18:18:16 +0200

    Created query handlers

---
+++ src/Services.Localisation.Infrastructure/Mongo/Queries/Handlers/GetLocationHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using MongoDB.Driver.Linq;
+using Services.Localisation.Application;
+using Services.Localisation.Application.DTO;
+using Services.Localisation.Application.Queries;
+using Services.Localisation.Infrastructure.Mongo.Documents;
+
+namespace Services.Localisation.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetLocationHandler: IQueryHandler<GetLocation, LocationDto>
+    {
+        private readonly IMongoRepository<LocationDocument, Guid> _locationRepository;
+
+        public GetLocationHandler(IMongoRepository<LocationDocument, Guid> locationRepository)
+        {
+            _locationRepository = locationRepository;
+        }
+
+        public async Task<LocationDto> HandleAsync(GetLocation query)
+        {
+            var document = await _locationRepository.GetAsync(p => p.Id == query.LocationId);
+
+            return document?.AsDto();
+        }
+    }
+}
---
+++ src/Services.Localisation.Infrastructure/Mongo/Queries/Handlers/GetLocationsHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Convey.CQRS.Queries;
+using Convey.Persistence.MongoDB;
+using MongoDB.Driver;
+using Services.Localisation.Application.DTO;
+using Services.Localisation.Application.Queries;
+using Services.Localisation.Infrastructure.Mongo.Documents;
+
+namespace Services.Localisation.Infrastructure.Mongo.Queries.Handlers
+{
+    public class GetLocationsHandler : IQueryHandler<GetLocations, IEnumerable<LocationDto>>
+    {
+        private readonly IMongoRepository<LocationDocument, Guid> _locationRepository;
+
+        public GetLocationsHandler(IMongoRepository<LocationDocument, Guid> locationRepository)
+        {
+            _locationRepository = locationRepository;
+        }
+
+        public async Task<IEnumerable<LocationDto>> HandleAsync(GetLocations query)
+        {
+            var documents = _locationRepository.Collection.AsQueryable();
+   
+            var locations = await documents.ToListAsync();
+
+            return locations.Select(p => Documents.Extensions.AsDto((LocationDocument) p));        
+        }
+    }
+}
