a9c79ba54e400a9f9d4597b29a3ffedbb31e9d21
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 13:17:55 +0200

    Created event

---
+++ src/Services.Localisation.Application/Events/LocationAdded.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Convey.CQRS.Events;
+
+namespace Services.Localisation.Application.Events
+{
+    [Contract]
+    public class LocationAdded: IEvent
+    {
+        public Guid LocationId { get; }
+        
+        public LocationAdded(Guid locationId)
+        {
+            LocationId = locationId;
+        }
+    }
+}
