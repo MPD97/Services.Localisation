0fb9b10d61638d73a9802994af8d523255c5d574
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 18:15:31 +0200

    Created repositories

---
+++ src/Services.Localisation.Infrastructure/Mongo/Repositories/LocationMongoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.Localisation.Core.Entities;
+using Services.Localisation.Core.Repositories;
+using Services.Localisation.Infrastructure.Mongo.Documents;
+
+namespace Services.Localisation.Infrastructure.Mongo.Repositories
+{
+    public class LocationMongoRepository : ILocationRepository
+    {
+        private readonly IMongoRepository<LocationDocument, Guid> _repository;
+
+        public LocationMongoRepository(IMongoRepository<LocationDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+        
+        public async Task<Location> GetAsync(Guid id)
+        {
+            var document = await _repository.GetAsync(l => l.Id == id);
+
+            return document?.AsEntity();
+        }
+        
+        public async Task<Location> GetByUserAsync(Guid userId)
+        {
+            var document = await _repository.GetAsync(l => l.UserId == userId);
+
+            return document?.AsEntity();
+        }
+        
+        public Task AddAsync(Location location) =>
+            _repository.AddAsync(location.AsDocument());
+    }
+}
---
+++ src/Services.Localisation.Infrastructure/Mongo/Repositories/UserMongoRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.Persistence.MongoDB;
+using Services.Localisation.Core.Entities;
+using Services.Localisation.Core.Repositories;
+using Services.Localisation.Infrastructure.Mongo.Documents;
+
+namespace Services.Localisation.Infrastructure.Mongo.Repositories
+{
+    public class UserMongoRepository : IUserRepository
+    {
+        private readonly IMongoRepository<UserDocument, Guid> _repository;
+
+        public UserMongoRepository(IMongoRepository<UserDocument, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<bool> ExistsAsync(Guid id)
+            => _repository.ExistsAsync(c => c.Id == id);
+
+        public Task AddAsync(User user) 
+            => _repository.AddAsync(user.AsDocument());
+    }
+}
