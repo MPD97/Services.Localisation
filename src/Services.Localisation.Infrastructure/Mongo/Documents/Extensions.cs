cb17b7e7178fc793d455d1b09d75f90f28115013
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 18:17:55 +0200

    Created document extension

---
+++ src/Services.Localisation.Infrastructure/Mongo/Documents/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using Services.Localisation.Application.DTO;
+using Services.Localisation.Core.Entities;
+
+namespace Services.Localisation.Infrastructure.Mongo.Documents
+{
+    public static class Extensions
+    {
+        public static Location AsEntity(this LocationDocument document)
+            => new Location(document.Id, document.UserId, document.CreatedAt, document.Latitude, document.Longitude, document.Accuracy);
+
+        public static LocationDocument AsDocument(this Location entity)
+            => new LocationDocument
+            {
+                Id = entity.Id,
+                UserId = entity.UserId,
+                CreatedAt = entity.CreatedAt,
+                Longitude = entity.Longitude,
+                Latitude = entity.Latitude,
+                Accuracy = entity.Accuracy
+            };
+
+        public static LocationDto AsDto(this LocationDocument document)
+            => new LocationDto
+            {
+                Id = document.Id,
+                UserId = document.UserId,
+                CreatedAt = document.CreatedAt,
+                Longitude = document.Longitude,
+                Latitude = document.Latitude,
+                Accuracy = document.Accuracy
+            };
+
+        public static User AsEntity(this UserDocument document)
+            => new User(document.Id);
+
+        public static UserDocument AsDocument(this User entity)
+            => new UserDocument
+            {
+                Id = entity.Id
+            };
+    }
+}
