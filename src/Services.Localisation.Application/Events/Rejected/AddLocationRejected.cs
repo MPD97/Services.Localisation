1d537a0091602f4405dedff2bb9d0e182c12af6d
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 13:18:34 +0200

    Created rejected event

---
+++ src/Services.Localisation.Application/Events/Rejected/AddLocationRejected.cs
@@ -0,0 +1,17 @@
+﻿using Convey.CQRS.Events;
+
+namespace Services.Localisation.Application.Events.Rejected
+{
+    [Contract]
+    public class AddLocationRejected: IRejectedEvent
+    {
+        public string Reason { get; }
+        public string Code { get; }
+
+        public AddLocationRejected(string reason, string code)
+        {
+            Reason = reason;
+            Code = code;
+        }
+    }
+}
