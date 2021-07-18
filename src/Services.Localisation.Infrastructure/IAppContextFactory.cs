ac546965fa830d5132e8ed3010c5dfd65f86f8fa
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 18:16:43 +0200

    Updated extension

---
+++ src/Services.Localisation.Infrastructure/IAppContextFactory.cs
@@ -0,0 +1,9 @@
+﻿using Services.Localisation.Application;
+
+namespace Services.Localisation.Infrastructure
+{
+    public interface IAppContextFactory
+    {
+        IAppContext Create();
+    }
+}
