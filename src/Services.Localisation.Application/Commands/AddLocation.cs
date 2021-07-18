1d52f01458503239a5e20d327fd151debdb4f0ef
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 13:19:20 +0200

    Created command

---
+++ src/Services.Localisation.Application/Commands/AddLocation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Convey.CQRS.Commands;
+
+namespace Services.Localisation.Application.Commands
+{
+    [Contract]
+    public class AddLocation : ICommand
+    {
+        public Guid LocationId { get; }  
+        public Guid UserId { get; }  
+        public decimal Latitude { get; }
+        public decimal Longitude { get; }
+        public int  Accuracy { get; }
+
+        public AddLocation(Guid locationId, Guid userId, decimal latitude, decimal longitude, int accuracy)
+        {
+            LocationId = locationId == Guid.Empty ? Guid.NewGuid() : locationId;
+            UserId = userId;
+            Latitude = latitude;
+            Longitude = longitude;
+            Accuracy = accuracy;
+        }
+    }
+}
---
+++ src/Services.Localisation.Application/Commands/Handlers/AddLocationHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using Convey.CQRS.Commands;
+using Services.Localisation.Application.Events;
+using Services.Localisation.Application.Services;
+using Services.Localisation.Core.Entities;
+using Services.Localisation.Core.Exceptions;
+using Services.Localisation.Core.Repositories;
+
+namespace Services.Localisation.Application.Commands.Handlers
+{
+    public class AddLocationHandler: ICommandHandler<AddLocation>
+    {
+        private readonly ILocationRepository _locationRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageBroker _messageBroker;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public AddLocationHandler(ILocationRepository locationRepository, IUserRepository userRepository,
+            IMessageBroker messageBroker, IDateTimeProvider dateTimeProvider)
+        {
+            _locationRepository = locationRepository;
+            _userRepository = userRepository;
+            _messageBroker = messageBroker;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task HandleAsync(AddLocation command)
+        {
+            if (!(await _userRepository.ExistsAsync(command.UserId)))
+            {
+                throw new UserNotFoundException(command.UserId);
+            }
+
+            var location = new Location(command.LocationId, command.UserId, _dateTimeProvider.Now, command.Latitude, command.Longitude, command.Accuracy);
+            await _locationRepository.AddAsync(location);
+            await _messageBroker.PublishAsync(new LocationAdded(command.LocationId));
+        }
+    }
+}
