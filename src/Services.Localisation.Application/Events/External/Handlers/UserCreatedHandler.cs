1e3fcf9ea81cd03c8525964a15a472d944888774
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 13:18:13 +0200

    Created external event

---
+++ src/Services.Localisation.Application/Events/External/Handlers/UserCreatedHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Convey.CQRS.Events;
+using Services.Localisation.Application.Exceptions;
+using Services.Localisation.Core.Entities;
+using Services.Localisation.Core.Repositories;
+
+namespace Services.Localisation.Application.Events.External.Handlers
+{
+    public class UserCreatedHandler: IEventHandler<UserCreated>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserCreatedHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(UserCreated @event)
+        {
+            if (await _userRepository.ExistsAsync(@event.UserId))
+            {
+                throw new UserAlreadyExistsException(@event.UserId);
+            }
+
+            await _userRepository.AddAsync(new User(@event.UserId));
+        }
+    }
+}
---
+++ src/Services.Localisation.Application/Events/External/UserCreated.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Convey.CQRS.Events;
+using Convey.MessageBrokers;
+
+namespace Services.Localisation.Application.Events.External
+{
+    [Message("users")]
+    public class UserCreated: IEvent
+    {
+        public Guid UserId { get; }
+
+        public UserCreated(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+}
