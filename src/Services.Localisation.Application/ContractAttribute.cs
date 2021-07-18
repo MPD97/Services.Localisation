63673a5d21862b1ebfba6f72256aa55ade005a81
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 13:18:59 +0200

    Created context

---
+++ src/Services.Localisation.Application/ContractAttribute.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Services.Localisation.Application
+{
+    public class ContractAttribute : Attribute
+    {
+    }
+}
---
+++ src/Services.Localisation.Application/IAppContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.Localisation.Application
+{
+    public interface IAppContext
+    {
+        string RequestId { get; }
+        IIdentityContext Identity { get; }
+    }
+}
