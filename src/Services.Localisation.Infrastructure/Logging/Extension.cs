86b01e3ef3e29451248e7728653cbeaf51954fd5
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-18 18:17:39 +0200

    Created logging

---
+++ src/Services.Localisation.Infrastructure/Logging/Extension.cs
@@ -0,0 +1,21 @@
+﻿using Convey;
+using Convey.Logging.CQRS;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Localisation.Application.Commands;
+
+namespace Services.Localisation.Infrastructure.Logging
+{
+    internal static class Extensions
+    {
+        public static IConveyBuilder AddHandlersLogging(this IConveyBuilder builder)
+        {
+            var assembly = typeof(AddLocation).Assembly;
+            
+            builder.Services.AddSingleton<IMessageToLogTemplateMapper>(new MessageToLogTemplateMapper());
+            
+            return builder
+                .AddCommandHandlersLogging(assembly)
+                .AddEventHandlersLogging(assembly);
+        }
+    }
+}
---
+++ src/Services.Localisation.Infrastructure/Logging/MessageToLogTemplateMapper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Convey.Logging.CQRS;
+using Services.Localisation.Application.Commands;
+using Services.Localisation.Application.Events.External;
+using Services.Localisation.Application.Exceptions;
+
+namespace Services.Localisation.Infrastructure.Logging
+{
+    internal sealed class MessageToLogTemplateMapper : IMessageToLogTemplateMapper
+    {
+        private static IReadOnlyDictionary<Type, HandlerLogTemplate> MessageTemplates 
+            => new Dictionary<Type, HandlerLogTemplate>
+            {
+                {
+                    typeof(AddLocation),     
+                    new HandlerLogTemplate
+                    {
+                        After = "Added a location with id: {LocationId} the users: {UserId}"
+                    }
+                },
+                {
+                    typeof(UserCreated),     
+                    new HandlerLogTemplate
+                    {
+                        After = "Added a user with id: {UserId}.",
+                        OnError = new Dictionary<Type, string>
+                        {
+                            {
+                                typeof(UserAlreadyExistsException), "User with id: {UserId} already exists."
+                            }
+                        }
+                    }
+                }
+            };
+        
+        public HandlerLogTemplate Map<TMessage>(TMessage message) where TMessage : class
+        {
+            var key = message.GetType();
+            return MessageTemplates.TryGetValue(key, out var template) ? template : null;
+        }
+    }
+}
