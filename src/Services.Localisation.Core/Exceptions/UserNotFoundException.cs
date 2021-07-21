4b593ec1637498d7b8b3ecbdf1d4c4ee486f7efe
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-21 11:26:22 +0200

    Crated exceptions

---
+++ src/Services.Localisation.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Services.Localisation.Core.Exceptions
+{
+    public class UserNotFoundException : DomainException
+    {
+        public override string Code { get; } = "user_not_found";
+        public Guid UserId { get; }
+        public UserNotFoundException(Guid userId) : base($"User with id: {userId} was not found.")
+        {
+            UserId = userId;
+        }
+    }
+}
