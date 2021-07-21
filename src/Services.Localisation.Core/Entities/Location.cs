d76c96cdb926c8ec6211b0ca78bea2b404a49c00
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-21 11:25:05 +0200

    Updated entities

--- src/Services.Localisation.Core/Entities/Location.cs
+++ src/Services.Localisation.Core/Entities/Location.cs
@@ -10,7 +10,7 @@ namespace Services.Localisation.Core.Entities
         public DateTime CreatedAt { get; private set; }
         public decimal Latitude { get; private set; }
         public decimal Longitude { get; private set; }
-        public int  Accuracy { get; private set; }
+        public int Accuracy { get; private set; }
 
         public Location(Guid id, Guid userId, DateTime createdAt, decimal latitude, decimal longitude, int accuracy)
         {
@@ -22,11 +22,11 @@ namespace Services.Localisation.Core.Entities
             Accuracy = IsValidAccuracy(accuracy) ? accuracy : throw new InvalidAccuracyException(userId, accuracy);
         }
 
-        public bool IsValidLatitude(decimal latitude)
+        public static bool IsValidLatitude(decimal latitude)
             => latitude is >= -90m and <= 90m;
-        public bool IsValidLongitude(decimal longitude)
+        public static bool IsValidLongitude(decimal longitude)
             => longitude is >= -180m and <= 180m;
-        public bool IsValidAccuracy(int accuracy)
+        public static bool IsValidAccuracy(int accuracy)
             => accuracy >= 1 ;
     }
 }
--- src/Services.Localisation.Core/Entities/User.cs
+++ src/Services.Localisation.Core/Entities/User.cs
@@ -5,10 +5,16 @@ namespace Services.Localisation.Core.Entities
     public class User
     {
         public Guid Id { get; private set; }
+        
+        public State State { get; private set; }
 
-        public User(Guid id)
+        public User(Guid id, State state)
         {
             Id = id;
+            State = state;
         }
+        
+        public void ChangeState(State state)
+            => State = state;
     }
 }
