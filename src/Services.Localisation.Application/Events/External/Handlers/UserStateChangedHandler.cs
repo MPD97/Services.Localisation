5f9074445cf3ae4d01d80b733c8991a7721ebd09
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-07-21 11:26:39 +0200

    Created events

---
+++ src/Services.Localisation.Application/Events/External/Handlers/UserStateChangedHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Convey.CQRS.Events;
+using Services.Localisation.Application.Exceptions;
+using Services.Localisation.Core.Entities;
+using Services.Localisation.Core.Exceptions;
+using Services.Localisation.Core.Repositories;
+
+namespace Services.Localisation.Application.Events.External.Handlers
+{
+    public class UserStateChangedHandler : IEventHandler<UserStateChanged>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserStateChangedHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task HandleAsync(UserStateChanged @event)
+        {
+            var user = await _userRepository.GetAsync(@event.UserId);
+            if (user is null)
+            {
+                throw new UserNotFoundException(@event.UserId);
+            }
+
+            if (!Enum.TryParse<State>(@event.CurrentState, true, out var state))
+            {
+                throw new CannotChangeUserStateException(@event.UserId, State.Unknown);
+            }
+
+            if (user.State == state)
+            {
+                return;
+            }
+
+            user.ChangeState(state);
+            await _userRepository.UpdateAsync(user);
+        }
+    }
+}
